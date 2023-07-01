946101462822a63ad2872c0c85125566e4fa079d
Piotr Rachwalik <piotr.rachwalik@gmail.com>
2023-07-01 20:56:48 +0200

    Ignore .history and add new test cases for OldProductTest

--- src/test/java/legacyfigher/dietary/newproducts/OldProductTest.java
+++ src/test/java/legacyfigher/dietary/newproducts/OldProductTest.java
@@ -11,8 +11,8 @@ import static org.junit.jupiter.api.Assertions.fail;
 class OldProductTest {
 
     // 1. fajnie by bylo miec obsluzone przypadki brzegowe dla wartosci pol
-    //  - max wartosc
-    //  - min wartosc, etc.
+    // - max wartosc
+    // - min wartosc, etc.
 
     // 2. trzeba potestowac wyjatki z tych metod
 
@@ -95,6 +95,56 @@ class OldProductTest {
     }
 
     @Test
+    void incrementCounterShouldThrowIllegalStateExceptionWhenPriceIsInvalid() {
+        OldProduct oldProduct1 = new OldProduct(null, "testDesc", "testLongDesc", 1);
+
+        try {
+            oldProduct1.incrementCounter();
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals("Invalid price", e.getMessage());
+        }
+
+        OldProduct oldProduct2 = new OldProduct(new BigDecimal(0), "testDesc", "testLongDesc", 1);
+
+        try {
+            oldProduct2.incrementCounter();
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals("Invalid price", e.getMessage());
+        }
+
+        OldProduct oldProduct3 = new OldProduct(new BigDecimal(-1), "testDesc", "testLongDesc", 1);
+        try {
+            oldProduct3.incrementCounter();
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals("Invalid price", e.getMessage());
+        }
+    }
+
+    @Test
+    void incrementCounterShouldThrowIllegalStateExceptionWhenCounterIsNullOrBelowZero() {
+        OldProduct oldProduct1 = new OldProduct(new BigDecimal(1), "test", "test", null);
+
+        try {
+            oldProduct1.incrementCounter();
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals("null counter", e.getMessage());
+        }
+
+        OldProduct oldProduct2 = new OldProduct(new BigDecimal(1), "test", "test", -2);
+
+        try {
+            oldProduct2.incrementCounter();
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals("Negative counter", e.getMessage());
+        }
+    }
+
+    @Test
     void productPriceShouldBeChangedFromValue12To10() {
         oldProduct = createDefaultOldProduct();
 
@@ -103,6 +153,44 @@ class OldProductTest {
         oldProduct.changePriceTo(new BigDecimal("10.00"));
 
         assertEquals(new BigDecimal("10.00"), oldProduct.price);
+    }
+
+    @Test
+    void productPriceShouldNotBeChangedIfCounterIsNullAndIllegalStateExceptionShouldBeThrown() {
+        oldProduct = new OldProduct(new BigDecimal(1), "test", "test", null);
+
+        try {
+            oldProduct.changePriceTo(new BigDecimal(0));
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals("null counter", e.getMessage());
+            assertEquals(new BigDecimal(1), oldProduct.price);
+        }
+    }
+
+    @Test
+    void productPriceShouldNotBeChangedIfCounterIsBelowZero() {
+        oldProduct = new OldProduct(new BigDecimal(1), "test", "test", -1);
+
+        assertEquals(new BigDecimal(1), oldProduct.price);
+
+        oldProduct.changePriceTo(new BigDecimal(2));
+
+        assertEquals(new BigDecimal(1), oldProduct.price);
+    }
+
+    @Test
+    void productPriceShouldNotBeChangedWhenCounterIsAboveZeroButNewPriceIsNull() {
+        oldProduct = new OldProduct(new BigDecimal(1), "test", "test", 1);
+
+        assertEquals(new BigDecimal(1), oldProduct.price);
+
+        try {
+            oldProduct.changePriceTo(null);
+        } catch (IllegalStateException e) {
+            assertEquals("new price null", e.getMessage());
+            assertEquals(new BigDecimal(1), oldProduct.price);
+        }
     }
 
     @Test
